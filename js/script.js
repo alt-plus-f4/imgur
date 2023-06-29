d7206ebb7cc74709b328cd7757d09e466b5a67f4
alt-plus-f4 <valentin.assenov@abv.bg>
2023-06-30 02:36:59 +0300

    login, signup, add post

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-      if($(this).scrollTop() > 1) { 
-          $('.navbar').addClass('solid');
-      } else {
-          $('.navbar').removeClass('solid');
-      }
-    });
+const popupScreen = document.querySelector("#popup-screen");
+const closeBtn = document.querySelector(".close-btn");
+
+window.onscroll = function() {
+    var element = document.getElementById("navbar");
+
+    if(scrollY < 230){
+        element.style = "background: transparent;";
+    }
+    else{
+        element.style = "background: #171544;";
+    }
+};
+
+window.addEventListener("load", function() {
+    var preloader = document.getElementById("preloader");
+    preloader.classList.add("hide");
 });
+
+function newPost(){
+    popupScreen.classList.add("active");
+}
+
+closeBtn.addEventListener("click", () => {
+    popupScreen.classList.remove("active");
+});
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     var preloader = document.getElementById("preloader");
+//     preloader.classList.add("hide");
+// });
+
+// function getRandomColor() {
+//     var letters = '0123456789ABCDEF';
+//     var color = '#';
+//     for (var i = 0; i < 6; i++) {
+//       color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+// }
+  
